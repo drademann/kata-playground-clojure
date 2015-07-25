29478b71f152ff781a583941f0dfd786ef305991
Dirk Rademann <contact@beyondworlds.net>
2015-07-25 21:57:31 +0200

    Created Leiningen project setup.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject kata-playground-clojure "0.1.0-SNAPSHOT"
+	:dependencies [[org.clojure/clojure "1.7.0"]]
+	:target-path "target/%s"
+	:profiles {:uberjar {:aot :all}})
---
+++ src/kata/primefactors.clj
@@ -0,0 +1,7 @@
+(ns kata.primefactors
+	(:gen-class))
+
+(defn generate-primefactors
+	"Generates the prime factors of a number."
+	[n]
+	())
