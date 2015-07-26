6680ec11306fdf77453f4a296e1ed9fdbcc53085
Dirk Rademann <contact@beyondworlds.net>
2015-07-26 15:22:01 +0200

    Added description and URL to leiningen project file.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject kata-playground-clojure "0.1.0-SNAPSHOT"
+	:description "Kata Playground for Clojure"
+	:url "http://github.com/drademann/kata-playground-clojure"
 	:dependencies [[org.clojure/clojure "1.7.0"]]
 	:target-path "target/%s"
 	:profiles {:uberjar {:aot :all}})
