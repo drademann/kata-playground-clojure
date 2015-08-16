b129320064979fd3f20747c3e1664b841eb6d08e
Dirk Rademann <contact@beyondworlds.net>
2015-08-16 10:58:30 +0200

    Code style adjusted.

--- test/kata/string_calculator.clj
+++ test/kata/string_calculator.clj
@@ -41,12 +41,8 @@
 
 (defn calc
 	[s]
-	(letfn [(delim
-						[d]
-						(re-pattern (str "[" d "\n]")))
-					(sumUp
-						[s d]
-						(reduce + (map #(Integer/parseInt %) (str/split (str/trim s) (delim d)))))]
+	(letfn [(delim [d] (re-pattern (str "[" d "\n]")))
+					(sumUp [s d] (reduce + (map #(Integer/parseInt %) (str/split (str/trim s) (delim d)))))]
 		(cond (empty? s) 0
 					(.startsWith s "//") (sumUp (.substring s 3) (.charAt s 2))
 					:else (sumUp s ","))))
