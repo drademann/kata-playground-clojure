ec67d1707ca61f1ade98c70d315a136c9fe10ee2
Dirk Rademann <dirk.rademann@icloud.com>
2018-03-22 23:33:20 +0100

    Updates to Clojure 1.9.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kata-playground-clojure "0.1.0-SNAPSHOT"
-	:description "Kata Playground for Clojure"
-	:url "http://github.com/drademann/kata-playground-clojure"
-	:dependencies [[org.clojure/clojure "1.8.0"]]
-	:target-path "target/%s"
-	:profiles {:uberjar {:aot :all}})
+  :description "Kata Playground for Clojure"
+  :url "http://github.com/drademann/kata-playground-clojure"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
